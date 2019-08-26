2c07bcee01289741ced1a8921dba622811bcc0fe
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-26 16:32:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,4 +6,12 @@ def sort_array_desc(int_array)
   intergers.sort.reverse
 end
 
-def 
+def sort_array_char_count(string)
+  string.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
